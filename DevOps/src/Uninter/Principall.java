090e70ae9a0e3c57fd2f88c2d5e19e551d082da8
Your Name <you@example.com>
2024-07-01 21:24:00 -0300

    Adicionando o codigo no repositorio

---
+++ DevOps/src/Uninter/Principall.java
@@ -0,0 +1,14 @@
+package Uninter;
+
+public class Principall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		soma soma = new soma(34,56);
+		
+		soma.somatorio();
+		System.out.println(soma.somatorio());
+	}
+
+}
---
+++ DevOps/src/Uninter/soma.java
@@ -0,0 +1,17 @@
+package Uninter;
+
+public class soma {
+	int n1;
+	int n2;
+	
+	public soma(int n1, int n2) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	
+	public int somatorio() {
+		return n1 + n2;
+	}
+	
+}
